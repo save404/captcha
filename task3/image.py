53db8af03105e8aa240de9d05a2b07ad7e92591a
ZYSzys <17367077526@163.com>
2018-04-09 20:53:41 +0800

    upd fonts;

--- task3/image.py
+++ task3/image.py
@@ -22,7 +22,7 @@ except ImportError:
     wheezy_captcha = None
 
 DATA_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
-DEFAULT_FONTS = [os.path.join(DATA_DIR, 'Arial.ttf')]
+DEFAULT_FONTS = [os.path.join(DATA_DIR, 'algerianef.ttf')]
 
 if wheezy_captcha:
     __all__ = ['ImageCaptcha', 'WheezyCaptcha']
